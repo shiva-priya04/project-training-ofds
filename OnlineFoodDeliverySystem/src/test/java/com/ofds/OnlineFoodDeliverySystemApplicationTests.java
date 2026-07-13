692bf1295ab04904fb5132537e4420a70b6e9aab
2507180 <2507180@LTIN707693.cts.com>
2026-07-13 17:04:36 +0530

    Spring boot project created

---
+++ OnlineFoodDeliverySystem/src/test/java/com/ofds/OnlineFoodDeliverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodDeliverySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
