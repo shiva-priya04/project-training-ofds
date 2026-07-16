68c9b0382fbe19da17a9232cbfd032e691eebb6e
2507180 <2507180@LTIN707693.cts.com>
2026-07-16 09:26:35 +0530

    Annotations added

--- OnlineFoodDeliverySystem/src/main/java/com/ofds/repository/CustomerRepository.java
+++ OnlineFoodDeliverySystem/src/main/java/com/ofds/repository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.ofds.repository;
 
 import com.ofds.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
 
 }
