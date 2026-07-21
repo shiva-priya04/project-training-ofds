267cc5b618ecc744f68dba7b7d970b69512a9bff
2507180 <2507180@LTIN707693.cts.com>
2026-07-21 16:57:58 +0530

    slight change in orders 😅

--- OnlineFoodDeliverySystem/src/main/java/com/ofds/service/OrdersServiceImpl.java
+++ OnlineFoodDeliverySystem/src/main/java/com/ofds/service/OrdersServiceImpl.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ofds.entity.MenuItem;
 import com.ofds.entity.Orders;
+import com.ofds.repository.MenuItemRepository;
 import com.ofds.repository.OrdersRepository;
 
 @Service
@@ -14,10 +16,20 @@ public class OrdersServiceImpl implements OrdersService {
 	
 	@Autowired
 	private OrdersRepository ordersRepository;
+	
+	@Autowired
+	private MenuItemRepository menuItemRepository;
 
 	@Override
 	public Orders saveOrder(Orders order) {
 		
+		double totalAmt = 0;
+		for(MenuItem item : order.getMenus()) {
+			MenuItem dbItem = menuItemRepository.findById(item.getItemId()).orElseThrow(() -> new RuntimeException("Menu Item not found: " + item.getItemId()));
+
+        totalAmt += dbItem.getPrice();
+		}
+		order.setTotalAmt(totalAmt);
 		return ordersRepository.save(order);
 	}
 
